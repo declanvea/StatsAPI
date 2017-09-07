e07bc43eab113650a212bae4179188f0559eae12
Declan Vea <declanpvea@gmail.com>
2017-09-07 17:04:50 -0400

    final

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const router = express.Router();
+const routes = require('./routes');
+const models = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(routes);
 
-router.get('/', (req, res) => {
-    res.json({ message: 'Camp Hope' });
-});
-
-app.use('/api', router);
-
+// entering in initial data for testing
+console.log(models.Activity);
+// models.Activity.create({
+//   exercise: 'Ate Donuts',
+//   amount: 7,
+//   date: 2004-08-10
+// }).then(function(){
+//   return models.Activity.findAll();
+// }).then(function(apple){
+//   console.log(apple.map(function(apple){
+//     return apple.exercise;
+//   }));
+// });
 
 app.listen(3000, () => console.log('Lunch has been cancelled, due to lack of hustle...'));
--- routes.js
+++ routes.js
@@ -2,39 +2,116 @@ const express = require('express');
 const router = express.Router();
 const models = require("./models");
 
-// Show a list of all activities I am tracking, and links to their individual pages
-router.get('/activities', (req, res) => {
-
+// // Show a list of all activities I am tracking, and links to their individual pages
+router.route('/activities')
+.get((req, res) => {
+  models.Activity.findAll().then(function(err, activity) {
+    if(err){
+      res.send(err);
+    } else {
+      res.json({activity:exercise});
+    }
+  })
 });
 
-// Create a new activity for me to track.
-router.post('/activities', (req, res) => {
-
+// // Create a new activity for me to track.
+router.route('/activities')
+.post((req, res) => {
+  let newExercise = {
+    exercise: req.body.exercise,
+    amount: req.body.amount
+  };
+  models.Activity.create(newExercise).then(function(err, activity){
+    if(err){
+      res.send(err);
+    } else {
+      res.json({message: 'created stuff'});
+    }
+  })
+  console.log(newExercise);
+});
+//
+// // Show information about one activity I am tracking, and give me the data I have recorded for that activity.
+router.route('/activities/:id')
+.get((req, res) => {
+  let id = req.params.id;
+  models.Activity.findById(id).then(function(err, activity){
+    if(err){
+      res.send(err);
+    } else {
+      res.json({activity});
+    }
+  })
+});
+//
+// // Update one activity I am tracking, changing attributes such as name or type. Does not allow for changing tracked data.
+router.route('/activities/:id')
+.put((req, res) => {
+  models.Activity.update({
+    exercise: req.body.exercise
+  },{
+    where: {
+      id: req.params.id
+    }
+  }).then(function(err, activity){
+    if(err){
+      res.send(err);
+    } else {
+      res.json({message: 'Updated activity'});
+    }
+  })
+});
+//
+// // Delete one activity I am tracking. This should remove tracked data for that activity as well.
+router.route('/activities/:id')
+.delete((req, res) => {
+  models.Activity.destroy({
+    where: {
+      id: req.params.id
+    }},{
+      exercise: req.body.exercise,
+      amount: req.body.amount
+    }).then(function(err, activity){
+    if(err){
+      res.send(err);
+    } else {
+      res.json({message: 'Deleted activity'});
+    }
+  })
+});
+//
+// // Add tracked data for a day. The data sent with this should include the day tracked. You can also override the data for a day already recorded.
+router.route('/activities/:id/stats')
+.post((req, res) => {
+let newUpdate = {
+  exercise: req.body.exercise,
+  amount: req.body.amount,
+  date: req.body.date
+}
+models.Activity.create({
+  where: {
+    id: req.params.id
+  }
+}, newUpdate).then(function(err, activity){
+  if(err){
+    res.send(err);
+  } else {
+    res.json({message: 'Replaced activity'});
+  }
+})
+});
+//
+// // Remove tracked data for a day.
+router.route('/stats')
+.delete((req, res) => {
+  models.Activity.destroy({
+    where: {
+      date: req.query.date
+    }}).then(function(activity){
+      res.json({message: 'Deleted activity'});
+    })
 });
 
-// Show information about one activity I am tracking, and give me the data I have recorded for that activity.
-router.get('/activities/{id}', (req, res) => {
-
-});
-
-// Update one activity I am tracking, changing attributes such as name or type. Does not allow for changing tracked data.
-router.put('/activities/{id}', (req, res) => {
-
-});
-
-// Delete one activity I am tracking. This should remove tracked data for that activity as well.
-router.delete('/activities/{id}', (req, res) => {
-
-});
-
-// Add tracked data for a day. The data sent with this should include the day tracked. You can also override the data for a day already recorded.
-router.post('/activities/{id}/stats', (req, res) => {
-
-});
-
-// Remove tracked data for a day.
-router.post('/stats,{id}', (req, res) => {
-
-});
+router.use('/api', router);
 
 module.exports = router;
